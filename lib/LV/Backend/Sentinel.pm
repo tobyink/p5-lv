5cbcc2f214cfac9214c4597cfe57cb01889e330a
Toby Inkster <mail@tobyinkster.co.uk>
2013-11-02 19:24:09 +0000

    eliminate goto from Sentinel

--- lib/LV/Backend/Sentinel.pm
+++ lib/LV/Backend/Sentinel.pm
@@ -14,10 +14,8 @@ sub lvalue :lvalue
 	my %args = @_;
 	my $caller = (caller(1))[3];
 	$args{get} ||= sub { "$caller is writeonly" };
-	$args{set} ||= sub { "$caller is readonly" };
-	
-	@_ = %args;
-	goto \&sentinel;
+	$args{set} ||= sub { "$caller is readonly" };	
+	sentinel(%args);
 }
 
 1;
